6413242e83439e39c5e7a47a393a51b8bf9f4bc3
Octave Larose <octave.larose@hotmail.fr>
2021-04-19 14:37:27 +0000

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xbootclasspath/a:./target/javacg-0.1-SNAPSHOT-dycg-agent.jar:benchmarks.jar -javaagent:./target/javacg-0.1-SNAPSHOT-dycg-agent.jar="incl=a;" -classpath benchmarks.jar Harness DeltaBlue 3
+java -Xbootclasspath/a:./target/javacg-0.1-SNAPSHOT-dycg-agent.jar:benchmarks.jar -javaagent:./target/javacg-0.1-SNAPSHOT-dycg-agent.jar="incl=.*;excl=[java|sun|gr].*" -classpath benchmarks.jar Harness DeltaBlue 3
