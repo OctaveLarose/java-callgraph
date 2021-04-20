5a2c623841018dc4630c778a20de7f05d8f04888
Octave Larose <octave.larose@hotmail.fr>
2021-04-20 14:16:56 +0000

    Now ignores Harness and Run methods

--- run_on_benchmarks.sh
+++ run_on_benchmarks.sh
@@ -4,11 +4,11 @@ BENCHMARKS_DIR="$HOME/are-we-fast-yet/benchmarks/Java/src"
 
 BENCHMARK_NAMES=(`pushd $BENCHMARKS_DIR > /dev/null ; ls *.java | sed 's/\.java$\|^Harness|^Run//g' ; popd > /dev/null`)
 
-NBR_ITER=3
+NBR_ITER=1
 
 for NAME in "${BENCHMARK_NAMES[@]}"
 do
   echo $NAME
-  java -Xbootclasspath/a:./target/javacg-0.1-SNAPSHOT-dycg-agent.jar:benchmarks.jar -javaagent:./target/javacg-0.1-SNAPSHOT-dycg-agent.jar="incl=.*;excl=[java|sun|gr].*" -classpath benchmarks.jar Harness $NAME $NBR_ITER
+  java -Xbootclasspath/a:./target/javacg-0.1-SNAPSHOT-dycg-agent.jar:benchmarks.jar -javaagent:./target/javacg-0.1-SNAPSHOT-dycg-agent.jar="incl=.*;excl=[java|sun|gr|Harness|Run].*" -classpath benchmarks.jar Harness $NAME $NBR_ITER
   mv calltrace.txt calltrace_$NAME.txt
 done
