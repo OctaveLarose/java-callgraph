20c7b9baba8c58b98788c290b7d28d6594a60fec
Octave Larose <octave.larose@hotmail.fr>
2021-04-20 14:57:24 +0000

    More benchmark running script tweaking

--- run_on_benchmarks.sh
+++ run_on_benchmarks.sh
@@ -2,13 +2,13 @@
 
 BENCHMARKS_DIR="$HOME/are-we-fast-yet/benchmarks/Java/src"
 
-BENCHMARK_NAMES=(`pushd $BENCHMARKS_DIR > /dev/null ; ls *.java | sed 's/\.java$\|^Harness|^Run//g' ; popd > /dev/null`)
+BENCHMARK_NAMES=(`pushd $BENCHMARKS_DIR > /dev/null ; ls *.java | sed 's/\.java$\|^Harness|^Run|^Benchmark//g' ; popd > /dev/null`)
 
 NBR_ITER=1
 
 for NAME in "${BENCHMARK_NAMES[@]}"
 do
   echo $NAME
-  java -Xbootclasspath/a:./target/javacg-0.1-SNAPSHOT-dycg-agent.jar:benchmarks.jar -javaagent:./target/javacg-0.1-SNAPSHOT-dycg-agent.jar="incl=.*;excl=[java|sun|gr|Harness|Run].*" -classpath benchmarks.jar Harness $NAME $NBR_ITER
+  java -Xbootclasspath/a:./target/javacg-0.1-SNAPSHOT-dycg-agent.jar:benchmarks.jar -javaagent:./target/javacg-0.1-SNAPSHOT-dycg-agent.jar="incl=.*;excl=[java|sun|gr|Harness].*" -classpath benchmarks.jar Harness $NAME $NBR_ITER
   mv calltrace.txt calltrace_$NAME.txt
 done
