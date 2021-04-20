67ded42ff43c61a513229ab7b3c91b9f2d415d12
Octave Larose <octave.larose@hotmail.fr>
2021-04-20 14:12:59 +0000

    Updated benchmarks running script to omit Run and Harness files

--- run_on_benchmarks.sh
+++ run_on_benchmarks.sh
@@ -2,12 +2,13 @@
 
 BENCHMARKS_DIR="$HOME/are-we-fast-yet/benchmarks/Java/src"
 
-BENCHMARK_NAMES=(`pushd $BENCHMARKS_DIR > /dev/null ; ls *.java | sed 's/\.java$//g' ; popd > /dev/null`)
+BENCHMARK_NAMES=(`pushd $BENCHMARKS_DIR > /dev/null ; ls *.java | sed 's/\.java$\|^Harness|^Run//g' ; popd > /dev/null`)
 
 NBR_ITER=3
 
 for NAME in "${BENCHMARK_NAMES[@]}"
 do
-   java -Xbootclasspath/a:./target/javacg-0.1-SNAPSHOT-dycg-agent.jar:benchmarks.jar -javaagent:./target/javacg-0.1-SNAPSHOT-dycg-agent.jar="incl=.*;excl=[java|sun|gr].*" -classpath benchmarks.jar Harness $NAME $NBR_ITER
-   mv calltrace.txt calltrace_$NAME.txt
+  echo $NAME
+  java -Xbootclasspath/a:./target/javacg-0.1-SNAPSHOT-dycg-agent.jar:benchmarks.jar -javaagent:./target/javacg-0.1-SNAPSHOT-dycg-agent.jar="incl=.*;excl=[java|sun|gr].*" -classpath benchmarks.jar Harness $NAME $NBR_ITER
+  mv calltrace.txt calltrace_$NAME.txt
 done
